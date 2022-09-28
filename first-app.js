d14c445e135c4aba97ed7a824570164d70653518
LuisaSalhuaTincoso <lsalhuatincoso@gmail.com>
2022-09-27 21:28:54 -0500

    create server basic

---
+++ first-app.js
@@ -0,0 +1,14 @@
+// console.log("hello Luma, your welcome");
+//  const fs =require('fs');
+//  fs.writeFileSync("hello.txt","Hello luma, nodejs course is interesting");
+// let name ="Maria"
+//  const person = {
+//     name: 'Max',
+//     age: 29,
+//     greet:()=>{
+//         let name="Carlos"
+//       console.log('Hi, I am ' + this.name);
+//     }
+//   };
+  
+//   person.greet();
