7270b93e01d81d0a7aa4eb4d9cd816cef9693c63
LuisaSalhuaTincoso <lsalhuatincoso@gmail.com>
2022-10-11 19:46:50 -0500

    routing with express

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
  const express = require('express');
  const app = express();
 
- app.use((req,res,next)=>{
-    console.log("probando first middleware");
-     next()
- })
- 
- app.use((req,res,next)=>{
-    console.log("probando 2nd middleware");
-     res.send('HOLA JUAN');
- })
+//  app.use('/',(req,res,next)=>{
+//    console.log("siemprese ejecuta");//dependiendo de la ruta podria entrar aqui y luego a la ruta especifica
+//    next()
+// })
+//  app.use('/users',(req,res,next)=>{
+//     console.log("probando route user");
+//     res.send('HOLA user path');
+//  })
+
+app.use('/users',(req,res,next)=>{
+   console.log("test route user");
+   res.send('<h2>Hola es user path</h2>');
+})
+app.use('/',(req,res,next)=>{
+   console.log("forever run");//dependiendo de la ruta podria entrar aqui y luego a la ruta especifica
+   res.send('<h1>Hola es inicial path</h1>');
+  
+})
 
 app.listen(3000)
